abb6f0cfd32f923986a22f7dc80512d4158cfffe
Tony <grykatony@gmail.com>
2024-11-20 12:47:59 -0600

    update

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.wafflepanel.wafflepanel {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    requires org.jsoup;
+    requires rkon.core;
+
+    opens com.wafflepanel.wafflepanel to javafx.fxml;
+    exports com.wafflepanel.wafflepanel;
+}
